af00ef0e003865c7c7a1e59f62f800eb0eb695da
ArseneMayam <mayamarsene@yahoo.com>
2019-10-29 10:02:58 -0400

    Commit project

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+        private readonly IConfiguration config;
+
+        public AboutModel(IConfiguration config) {
+            this.config = config;
+        }
+
+        public void OnGet()
+        {
+            // Message = "Your application description page.";
+            Message = config["Message"];
+        }
+    }
+}
